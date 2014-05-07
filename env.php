233e8f14d1b8e343d77620bfa89ba0cd97fba1b4
Kemish W Hendershot <kemish@aptitudeit.net>
2014-05-07 17:07:14 +0000

    Added code for demo url to env.php.
    Signed-off-by: Kemish W Hendershot <kemish@aptitudeit.net>

--- env.php
+++ env.php
@@ -9,14 +9,25 @@ $email_destination_whitelist = array(
 $directory = array_pop(explode('/', dirname(dirname(dirname(__FILE__)))));
 
 if ($directory == 'aptitude') {
-	define('ENGINE_PROTECTED_PATH', '/home/aptitude/dev/cms2/protected');
-	define('ENGINE_PUBLIC_PATH', '/home/aptitude/cms2/public');
-	define('APP_PATH', dirname(__FILE__));
-	define('APP_PROTECTED_PATH', APP_PATH . "/protected");
-	define('APP_PUBLIC_PATH', APP_PATH . "/public");
-	$ENGINE_URL = "http://dev.aptitudecare.com/cms2-public";
-	$SITE_URL = "http://dev.aptitudecare.com";
-	$SECURE_CDN_URL = $SITE_URL;
+	if (file_exists(dirname(__FILE__) . "/.development")) {
+		define('ENGINE_PROTECTED_PATH', '/home/aptitude/dev/cms2/protected');
+		define('ENGINE_PUBLIC_PATH', '/home/aptitude/cms2/public');
+		define('APP_PATH', dirname(__FILE__));
+		define('APP_PROTECTED_PATH', APP_PATH . "/protected");
+		define('APP_PUBLIC_PATH', APP_PATH . "/public");
+		$ENGINE_URL = "http://dev.aptitudecare.com/cms2-public";
+		$SITE_URL = "http://dev.aptitudecare.com";
+		$SECURE_CDN_URL = $SITE_URL;
+	} else {
+		define('ENGINE_PROTECTED_PATH', '/home/aptitude/demo/cms2/protected');
+		define('ENGINE_PUBLIC_PATH', '/home/aptitude/demo/cms2/public');
+		define('APP_PATH', dirname(__FILE__));
+		define('APP_PROTECTED_PATH', APP_PATH . "/protected");
+		define('APP_PUBLIC_PATH', APP_PATH . "/public");
+		$ENGINE_URL = "http://demo.aptitudecare.com/cms2-public";
+		$SITE_URL = "http://demo.aptitudecare.com";
+		$SECURE_CDN_URL = $SITE_URL;
+	}
 } else {
 	if (file_exists(dirname(__FILE__) . "/.development")) {
 		define('ENGINE_PROTECTED_PATH', '/home/aptitude/sites/' . $directory . '/dev/cms2/protected');
